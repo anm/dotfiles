25c95e201c70ac3576c8ba24576cebd6a30ffa1b
Alistair MacLeod <alistair@biscuitsfruit.org.uk>
2011-04-30 22:34:12 +0100

    install script now works.

--- install.sh
+++ install.sh
@@ -6,10 +6,15 @@ fi
 
 for FILE in .[a-zA-Z_]*; do
     echo $FILE
+
+    if [ -h ~/$FILE ]; then
+        rm ~/$FILE
+    fi
+
     if [ -e ~/$FILE ]; then
         echo $FILE exists, backing up
         mv ~/$FILE replaced/
     fi
 
-    ln -s $FILE ~/$FILE
+    ln -s dotfiles/$FILE ~/$FILE
 done
